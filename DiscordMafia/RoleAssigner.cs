69c77c7cfedeb40fdf0142522d81411df74a7b7d
W <kgbcheb@ya.ru>
2017-05-31 00:10:30 +0300

    The algorithm for calculating the number of Mafia was changed: Before: `Math.Truncate(players.Count * (double)settings.MafPercent / 100)` = count(Mafioso) Now: `Math.Truncate(players.Count * (double)settings.MafPercent / 100)` = **count(Team.Mafia)**

--- DiscordMafia/RoleAssigner.cs
+++ DiscordMafia/RoleAssigner.cs
@@ -10,36 +10,39 @@ namespace DiscordMafia
         private Random random = new Random();
         IList<InGamePlayerInfo> players;
         private int remainingPlayerCount = 0;
+        private int remainingMafCount = 0;
 
         public void AssignRoles(IList<InGamePlayerInfo> players, Config.GameSettings settings)
         {
             this.players = players;
             remainingPlayerCount = players.Count;
-            var mafCount = Math.Truncate(players.Count * (double)settings.MafPercent / 100);
-            Console.WriteLine("Количество мафиози: {0}", mafCount);
+            remainingMafCount = (int)Math.Truncate(players.Count * (double)settings.MafPercent / 100);
+            Console.WriteLine("Количество мафиози: {0}", remainingMafCount);
 
             foreach (var player in players)
             {
                 player.startRole = player.role = new Citizen() { Player = player };
             }
 
-            for (int i = 0; i < mafCount; i++)
-            {
-                AssignRoleToRandomPlayer(new Mafioso());
-            }
             foreach (var role in settings.Roles.getTemporaryRoles(players.Count))
             {
                 AssignRoleToRandomPlayer(role);
             }
+
             foreach (var role in settings.Roles.getRandomRoles(players.Count, settings.MaxRandomPlayers, random))
             {
                 AssignRoleToRandomPlayer(role);
+            }
+
+            while (remainingMafCount > 0 && remainingPlayerCount > 0)
+            {
+                AssignRoleToRandomPlayer(new Mafioso());
             }
         }
 
         private void AssignRoleToRandomPlayer(BaseRole role)
         {
-            if (remainingPlayerCount == 0)
+            if (remainingPlayerCount <= remainingMafCount && role.Team != Team.Mafia || remainingPlayerCount == 0)
             {
                 Console.WriteLine("Недостаточно игроков для назначения роли {0}.", role.Name);
                 return;
@@ -59,6 +62,10 @@ namespace DiscordMafia
                 }
             }
             remainingPlayerCount--;
+            if (role.Team == Team.Mafia)
+            {
+                remainingMafCount--;
+            }
         }
     }
 }
